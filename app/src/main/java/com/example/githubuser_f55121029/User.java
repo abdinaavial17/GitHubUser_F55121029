b95a802e21362542a4f0406dbd7065bc82430aa5
abdinaavial17 <abdinaavial17@gmail.com>
2022-11-03 15:44:37 +0800

    Initial commit

---
+++ app/src/main/java/com/example/githubuser_f55121029/User.java
@@ -0,0 +1,25 @@
+package com.example.githubuser_f55121029;
+
+public class User {
+    public String getPhoto() {
+        return photo;
+    }
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getType() {
+        return type;
+    }
+    public void setType(String type) {
+        this.type = type;
+    }
+    private String photo;
+    private String name;
+    private String type;
+}
